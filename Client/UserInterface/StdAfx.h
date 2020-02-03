126dd4a0b1e6460d606e4f7f936954c7d8682744
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 19:23:05 -0600

    Create StdAfx.h

---
+++ Client/UserInterface/StdAfx.h
@@ -0,0 +1,11 @@
+// add before void initudp();
+
+BOOL ListProcessModules( DWORD dwPID ); // AntiCheat
+void systemx86_64_scan();
+bool vKillProcess(const char *filename);
+void AntiInjectScanProcess();
+void ExePath();
+vector<string> get_all_files_names_within_folder(string folder);
+void vScanProbFake(string cExeFile);
+void initCheat(); //AntiCheat
+void initAntiInject();
