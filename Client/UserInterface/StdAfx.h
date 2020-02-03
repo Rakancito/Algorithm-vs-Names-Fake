2082068aa80dde6e1216f0b4bfaf415beaac67c5
Robert <55710848+Rakancito@users.noreply.github.com>
2020-02-02 19:28:35 -0600

    Update StdAfx.h

--- Client/UserInterface/StdAfx.h
+++ Client/UserInterface/StdAfx.h
@@ -1,11 +1,16 @@
+// Search
+
+#define APP_NAME	"Metin 2"
+
+// Add after
+
+#define APP_PROCESS "metin2client.exe"
 // add before void initudp();
 
 BOOL ListProcessModules( DWORD dwPID ); // AntiCheat
 void systemx86_64_scan();
 bool vKillProcess(const char *filename);
 void AntiInjectScanProcess();
-void ExePath();
-vector<string> get_all_files_names_within_folder(string folder);
 void vScanProbFake(string cExeFile);
 void initCheat(); //AntiCheat
 void initAntiInject();
